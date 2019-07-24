69e6e6a59b665b12cad02b9becec7430531e315c
cruizsan <carlos.ruizsanchez@unamur.be>
2019-07-24 13:21:27 +0200

    fixing build-docker-image.sh name

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 dir=`dirname "$(realpath $0)"`
-name=services-documents-generator-other
+name=services-documents-generator
 version=0.0.1
 
 docker image build -t ${name}:${version} ${dir}
